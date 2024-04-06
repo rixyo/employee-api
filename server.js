0f5760cd596df90482a5400f458130f10b8f1cbe
rixyo <saleh35-3063@diu.edu.bd>
2024-04-06 08:27:25 +0600

    Add document in server.js

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
+// Importing express module
 const express = require("express");
+
+// Creating an express application instance
 const app = express();
+
+// Setting up the port to listen on, defaulting to 3002 if not provided
 const port = process.env.PORT || 3002;
+
+// Importing routes
 const departmentRoute = require("./routes/department.route");
 const jobRoute = require("./routes/job.route");
 const locationRoute = require("./routes/location.route");
 const employeeRoute = require("./routes/employee.route");
+
+// Middleware to parse incoming JSON requests
 app.use(express.json());
+
+// Middleware to parse incoming URL-encoded requests with extended options
 app.use(express.urlencoded({ extended: true }));
 
+// Mounting routes
 app.use("/api/departments", departmentRoute);
 app.use("/api/jobs", jobRoute);
 app.use("/api/locations", locationRoute);
 app.use("/api/employees", employeeRoute);
 
-
+// Starting the server to listen on the specified port
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
